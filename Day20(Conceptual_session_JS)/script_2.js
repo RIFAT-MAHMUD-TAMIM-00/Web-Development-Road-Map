f72ec373ade26efcfadd796d6163feb8d0625375
RIFAT-MAHMUD-TAMIM-00 <rifattamim1951@gmail.com>
2026-09-10 12:57:50 +0600

    add all the concetual learning

---
+++ Day20(Conceptual_session_JS)/script_2.js
@@ -0,0 +1,3 @@
+console.log("First Javascript conceptual session");
+
+var age = 25;
---
+++ Day20(Conceptual_session_JS)/type_conversion.js
@@ -0,0 +1,32 @@
+// Converted string to integer
+
+let age="25";
+let age2=Number(age); // converted to number
+
+let roll="26";
+let roll2=parseInt(roll); //2nd type conversion
+
+console.log(age2);
+console.log(roll2);
+
+
+// Converted number to string
+
+let houseNum=42;
+let houseNum2=String(houseNum);
+console.log(houseNum2);
+
+
+// Converted string to float
+
+
+let cgpa = "4.50";
+let cgpa2 = parseFloat(cgpa);
+console.log( typeof cgpa2);
+
+// limit float sums
+
+
+let floatSum=0.2+0.3;
+console.log(floatSum.toFixed(2));
+
